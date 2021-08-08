67320fec5111a42c823ec0ccea86b5c0d1568894
hafizulf <hafizulf68@gmail.com>
2021-08-09 00:35:24 +0700

    Add validation for max characters length of post

--- model/post.rb
+++ model/post.rb
@@ -9,7 +9,18 @@ class Post
     end
 
     def save
+        return chars_length_err unless message_valid?
+
         client = create_db_client
         client.query("INSERT INTO posts(message, tags) VALUES ('#{message}', '#{tags}') ")
+    end
+
+    def message_valid?
+        return false unless @message.length <= 1000
+        true
+    end
+
+    def chars_length_err
+        raise "You out of maximum characters length, try below 1000."
     end
 end
