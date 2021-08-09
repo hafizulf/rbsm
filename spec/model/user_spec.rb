aff6d098443ab758941aecf7801694037b01ca6b
hafizulf <hafizulf68@gmail.com>
2021-08-09 23:58:40 +0700

    Add unit test for User model - find_all method

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,21 +1,25 @@
 require_relative '../../model/user'
-require 'mysql2'
 
 RSpec.describe User do
-    context "#Get" do
-        it "- should return a kind of data" do
-            stub_client = double
-            allow(Mysql2::Client).to receive(:new).and_return(stub_client)
-            stub_query = "SELECT * FROM users"
+    before(:each) do
+        @client = double
+        @user = User.new(id: 10, username: "User", email: "user@exp.com", bio_desc: "Hello!")
+        allow(Mysql2::Client).to receive(:new).and_return(@client)
+    end
 
-            expect(stub_client).to receive(:query).with(stub_query)
-            allow(stub_client).to receive(:query).and_return([
-                {"username" => "skuyngoding", "email" => "skuy@ex.com", "bio_desc" => "Rediscover Yourself"}
-            ])
+    describe 'get users' do
+        context 'when find_all' do
+            it 'should return all users data' do
+                mock = "SELECT * FROM users"
+                stub = []
 
-            users = User.find_all
-            expect(users).to include(a_kind_of(User))
+                expect(@client).to receive(:query).with(mock).and_return(stub)
 
+                @user = User.find_all
+
+                # expect(@user).to eq(1)
+                expect(@user).to be_an Enumerator
+            end
         end
     end
 end
