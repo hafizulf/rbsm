c221c5d8383802b2b3d42d723a5664f68a41a745
hafizulf <hafizulf68@gmail.com>
2021-08-11 21:24:40 +0700

    Update test and code refactoring: save comment tags

--- model/comment.rb
+++ model/comment.rb
@@ -12,6 +12,19 @@ class Comment
         client = create_db_client
         client.query("INSERT INTO comments(post_id, comment) VALUES('#{post_id}', '#{comment}') ")
 
+        comment_id = client.last_id
+        tags = get_tags(@comment)
+
+        tags.each do |tag|
+            client.query("INSERT INTO comment_tags(comment_id, tag) VALUES('#{comment_id}', '#{tag}') ")
+        end
+
         true
+    end
+
+    def get_tags(param)
+        param = param.downcase.split(' ')
+        arr = param.select { |word| word.start_with?('#') }.uniq
+        arr.map {|a| a.delete_prefix('#')}
     end
 end
--- spec/model/comment_spec.rb
+++ spec/model/comment_spec.rb
@@ -11,9 +11,13 @@ RSpec.describe Comment do
         context "saving comment" do
             it "should return ok" do
                 mock = "INSERT INTO comments(post_id, comment) VALUES('22', 'new comment #new') "
+                mock2 = "INSERT INTO comment_tags(comment_id, tag) VALUES('22', 'new') "
 
                 allow(@client).to receive(:new).and_return(post_id: 22, comment: 'new post #new')
                 expect(@client).to receive(:query).with(mock)
+
+                allow(@client).to receive(:last_id).and_return(22)
+                expect(@client).to receive(:query).with(mock2)
 
                 expect(@comment.save).to eq(true)
             end
